ebbff7b1aa4b674d3dcd4c171a0d1dbcd555b81f
Labham Sharma <labhamsharma1633@gmail.com>
2025-09-26 20:22:59 +0530

    finished the navbar section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,22 +3,24 @@ import { Route,Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-
-
 export const serverUrl="http://localhost:8000"
 import {ToastContainer} from "react-toastify"
 import getCurrentUser from './customHooks/getCurrentUser'
+import { useSelector } from 'react-redux'
+import Profile from './pages/Profile'
 
 function App() {
   getCurrentUser()
+  const {userData}=useSelector(state=>state.user)
   
   return (
     <>
     <ToastContainer/>
     <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/signup' element={userData?<Signup/>:<Navigate to={"/"}/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/profile' element={userData?<Profile/>:<Navigate to={"/signup"}/>}/>
 
 
     </Routes>
